f8e588c602d1d99b0211a55474ebf7f06254d4d8
aniruddhatope <aniruddha.tope@gmail.com>
2024-02-04 20:31:46 +0530

    updated controller.java

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -14,6 +14,6 @@ public class TestController {
 
     @GetMapping("/message")
     public String getMessage() {
-        return "Second message from AWS Ecs";
+        return "another new message from AWS Ecs";
     }
 }
